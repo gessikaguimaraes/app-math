a63fe111cf79da7c5d9f9cbb2bbf23df5c14af63
Géssika <gessika.156@gmail.com>
2020-07-08 10:15:59 -0300

    Tela Splash Screen

--- app_math/lib/app/modules/splashScreen/splashScreen_page.dart
+++ app_math/lib/app/modules/splashScreen/splashScreen_page.dart
@@ -1,41 +1,73 @@
-import 'package:app_math/app/modules/home/home_page.dart';
+import 'dart:async';
+import 'dart:math' as math;
+
 import 'package:app_math/app/shared/const/images_const.dart';
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
 
 class SplashScreenPage extends StatefulWidget {
   @override
   _SplashScreenPageState createState() => _SplashScreenPageState();
 }
 
-class _SplashScreenPageState extends State<SplashScreenPage> {
+class _SplashScreenPageState extends State<SplashScreenPage>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5)).then((value) {
+      Navigator.pushNamed(context, "/home");
+    });
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _introScreen();
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      //backgroundColor: Color(0xff93291E),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Color(0xffED213A), Color(0xff93291E)],
+              ),
+            ),
+          ),
+          SpinningContainer(controller: _controller),
+        ],
+      ),
+    );
   }
 }
 
-Widget _introScreen() {
-  return Stack(
-    children: <Widget>[
-      SplashScreen(
-        seconds: 5,
-        gradientBackground: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [Color(0xffED213A), Color(0xff93291E)],
-        ),
-        navigateAfterSeconds: HomePage(),
-        loaderColor: Colors.transparent,
+class SpinningContainer extends AnimatedWidget {
+  const SpinningContainer({Key key, AnimationController controller})
+      : super(key: key, listenable: controller);
+
+  Animation<double> get _progress => listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: _progress.value * 2.0 * math.pi,
+      child: Container(
+        alignment: Alignment.center,
+        child: Image.asset(ImagesConst.favicon),
       ),
-      Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(ImagesConst.favicon),
-            fit: BoxFit.contain,
-          ),
-        ),
-      ),
-    ],
-  );
+    );
+  }
 }
